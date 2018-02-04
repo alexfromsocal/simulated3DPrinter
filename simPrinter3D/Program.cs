27b38ce8e5e51662742f3bdd34090a0e8e48c4c6
Mitchell Marsh <mmarsh@scale7.co>
2018-02-04 05:31:00 -0800

    Last commit before FRM

--- simPrinter3D/Program.cs
+++ simPrinter3D/Program.cs
@@ -8,6 +8,9 @@ using System.Net.Http.Headers;
 using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Security.Cryptography;
 
 namespace simPrinter3D
 {
@@ -27,37 +30,54 @@ namespace simPrinter3D
         {
             //Variable to use in place of zero index;
             const int frontIndex = 0;
-
+            string HashOfRealObject = "";
 
             Console.WriteLine("Hello World!");                                      //Entry point
 
 
             //Retrieves token key from user acquired from marketplace
             //Fetches data from 'Main' args param
-            string purchaseToken = SetTokenObject("RYY986559tDfg");                //String used for verification
-            string userIDToken = SetTokenObject("88HFGHF*di76UFGu6UYG");
+            string purchaseToken = SetTokenObject("AAF4D309-40F6-451E-BAE7-A0F174D98CEE"); // The Consumable Token
+            string userIDToken = SetTokenObject("2a7043cd-cad3-455d-875a-ef850ca1fdbd");
 
-            Guid singleUseAuthToken = TokenAuthentication(purchaseToken, userIDToken).Result;
+            var Approval = TokenAuthentication(purchaseToken, userIDToken).Result;
+            Guid singleUseAuthToken = Approval.auth_token;
+            HashOfRealObject = Approval.object_hash;
 
+            //This Auth token will be used to communicate with the 
 
 
             Console.WriteLine("Token Purchase Validated");
-            Object fileToPrint = ReadyToPrintEventHandler(singleUseAuthToken).Result;
+            Console.WriteLine("Getting Ready to print ...");
+            Thread.Sleep(2000); // For dramatic effect
 
+            //get the file from the server
+            Stream fileToPrint = ReadyToPrint(singleUseAuthToken).Result;
+            //Verify that the response was in fact the file to print
+            Console.WriteLine("Confirming Object...");
+            string resultHash = "";
+            using (MemoryStream rea = new MemoryStream())
+            {
+                fileToPrint.CopyTo(rea);
+                SHA256Managed sha = new SHA256Managed();
+                resultHash = BitConverter.ToString(sha.ComputeHash(rea.ToArray())).Replace("-", String.Empty);
+            }
+            if (resultHash == HashOfRealObject)
+            {
+                Console.WriteLine("Correct Object Received!");
+                Console.WriteLine("Printing Object...");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.WriteLine("Token is either expired or corrupted data is present.");
+
+            }
             //Do Something here that "prints" (probably save on computer)
             //This is a simulation after all. :D
 
-            Console.WriteLine("Invalid Purchase Token.");
-            Console.WriteLine("Please run again.");
-
-#if DEBUG
-            Console.ReadLine();
-#endif
 
         }
-
-
-
         //returns string that is passed in
         static string SetTokenObject(string e_token)
         {
@@ -72,14 +92,13 @@ namespace simPrinter3D
         /// </summary>
         /// <param name="e_token"></param>
         /// <returns></returns>
-        static async Task<Guid> TokenAuthentication(string p_token, string userToken)
+        static async Task<AuthTokenJson> TokenAuthentication(string p_token, string userToken)
         {
             HttpClient client = new HttpClient();
             var response = await client.GetAsync($"https://localhost:44358/printerapi/AuthenticatePrintToken?consumable_license={p_token}&user_id={userToken}");
-            //response.IsSuccessStatusCode
 
-            //
-            throw new NotImplementedException();
+            return JsonConvert.DeserializeObject<AuthTokenJson>(await response.Content.ReadAsStringAsync());
+
         }
 
         public static async Task<Guid> TokenSender(string p_token, string userToken)
@@ -94,47 +113,22 @@ namespace simPrinter3D
 
             return validated;
         }
-
-
-        /// <summary>
-        /// Signals to PAPI that the 3D printer has received PAPI handshake validation
-        /// Posts a request to PAPI to begin printing process.
-        /// </summary>
-        public static async Task<Object> ReadyToPrintEventHandler(Guid singleUseAuthToken)
-        {
-            Object fileToPrint = new Object();
-            try
-            {
-
-                fileToPrint = await ReadyToPrint(singleUseAuthToken);
-
-            }
-
-
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            return fileToPrint;
-        }
-
-        public static async Task<Object> ReadyToPrint(Guid singleUseAuthToken)
+        
+        public static async Task<Stream> ReadyToPrint(Guid singleUseAuthToken)
         {
             HttpClient client = new HttpClient();
-
-
             //REMEMBER TO CHANGE THE API URL
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/recordledger", singleUseAuthToken);
+            HttpResponseMessage response = await client.GetAsync($"https://localhost:44358/printerapi/AuthenticatedAndReadyToPrintRequest?auth_token_id={singleUseAuthToken}");
             response.EnsureSuccessStatusCode();
-            Object fileToPrint = await response.Content.ReadAsAsync<Guid>();
-            return fileToPrint;
-
+            return await response.Content.ReadAsStreamAsync();
         }
-
-
-
-
         //singleUseReceive func
 
+    }
+    public class AuthTokenJson
+    {
+        public Guid auth_token { get; set; }
+        public DateTime expiration { get; set; }
+        public string object_hash { get; set; }
     }
 }
